34840071b8cc577fadb25ada1d8f436f49a28c2f
d3y1 <d3y1_2u@163.com>
2024-02-28 11:28:22 +0800

    BD2 裁减网格纸: 贪心

---
+++ src/main/java/middle/BD2.java
@@ -0,0 +1,56 @@
+package middle;
+
+import java.util.Scanner;
+
+/**
+ * BD2 裁减网格纸
+ * @author d3y1
+ */
+public class BD2{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNext()){
+            solution(in);
+        }
+    }
+
+    /**
+     * 贪心
+     * @param in
+     */
+    private static void solution(Scanner in){
+        int n = in.nextInt();
+
+        int x,y;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for(int i=1; i<=n; i++){
+            x = in.nextInt();
+            y = in.nextInt();
+
+            minX = Math.min(minX, x);
+            maxX = Math.max(maxX, x);
+            minY = Math.min(minY, y);
+            maxY = Math.max(maxY, y);
+
+//                if(x < minX){
+//                    minX = x;
+//                }
+//                if(x > maxX){
+//                    maxX = x;
+//                }
+//                if(y < minY){
+//                    minY = y;
+//                }
+//                if(y > maxY){
+//                    maxY = y;
+//                }
+        }
+
+        int edge = Math.max(maxX-minX, maxY-minY);
+        System.out.println(edge*edge);
+    }
+}
